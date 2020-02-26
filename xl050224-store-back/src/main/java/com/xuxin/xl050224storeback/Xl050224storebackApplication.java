bcae01d82b3f2a514a19dd130f12a6b94cedef38
昕 <1046571324@qq.com>
2020-02-26 14:07:17 +0800

    xin0226@:store-back

---
+++ xl050224-store-back/src/main/java/com/xuxin/xl050224storeback/Xl050224storebackApplication.java
@@ -0,0 +1,13 @@
+package com.xuxin.xl050224storeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xl050224storebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Xl050224storebackApplication.class, args);
+    }
+
+}
