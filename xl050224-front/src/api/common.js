7a1944ad66a0c20bfcdc61c497a026ade7f7d598
昕 <1046571324@qq.com>
2020-02-27 11:15:05 +0800

    xin0227@:前端

---
+++ xl050224-front/src/api/common.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8090/'
+axios.interceptors.request.use(
+    config => {
+        if (window.sessionStorage.getItem('token')) { // 判断是否存在token，如果存在的话，则每个http header都加上token
+            config.headers.authorization = window.sessionStorage.getItem('token');  //请求头加上token
+        }
+        return config;
+    },
+    err => {
+        return Promise.reject(err)
+    })
+export default axios;
