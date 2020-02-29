19f95de01c97263481b93eeea4cf6225836ab0ae
昕 <1046571324@qq.com>
2020-02-29 18:25:24 +0800

    xin0229@:创建store-front

---
+++ xl050224-store-front/src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+Vue.use(ElementUI);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
