873cd8416ce3fafa0d9c3d79dee50beed9c66036
昕 <1046571324@qq.com>
2020-03-04 20:53:10 +0800

    xin0304@:store-back customer login 完善

---
+++ xl050224-store-back/src/main/java/com/xuxin/xl050224storeback/filter/LoginFilter.java
@@ -0,0 +1,73 @@
+package com.xuxin.xl050224storeback.filter;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.xuxin.xl050224storeback.constant.ClientExceptionConstant;
+import com.xuxin.xl050224storeback.exception.ClientException;
+import com.xuxin.xl050224storeback.util.JWTUtil;
+import com.xuxin.xl050224storeback.vo.CustomerLoginVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Set;
+
+@Order(2)
+@Component
+public class LoginFilter implements Filter {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Value("${jwt.exclude.apiUrls}")
+    private Set<String> excludeLoginApiUrls;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        final String method = request.getMethod();
+        if (method.equals("OPTIONS")){
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        final String requestURI = request.getRequestURI();
+
+        if (excludeLoginApiUrls.contains(requestURI)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        String token = request.getHeader("jcartToken");
+//        if (token == null || token.isEmpty()){
+//            token = request.getParameter("jcartToken");
+//        }
+        if (token == null || token.isEmpty()) {
+            throw new ClientException(ClientExceptionConstant.TOKEN_NOT_EXIST_ERRCODE, ClientExceptionConstant.TOKEN_NOT_EXIST_ERRMSG);
+        }
+
+        logger.info("verify login with token: {}", token);
+
+        CustomerLoginVO administratorLoginVO = null;
+        try {
+            administratorLoginVO = jwtUtil.verifyToken(token);
+        }catch (JWTVerificationException ex){
+            throw new ClientException(ClientExceptionConstant.TOKEN_INVALID_ERRCODE, ex.getMessage());
+        }
+
+        request.setAttribute("customerId", administratorLoginVO.getCustomerId());
+        request.setAttribute("customerUsername", administratorLoginVO.getUsername());
+
+        filterChain.doFilter(servletRequest, servletResponse);
+        return;
+    }
+}
