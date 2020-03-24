06507edb52db5b4eae66c6a65e44a4fb54f0abff
昕 <1046571324@qq.com>
2020-03-24 16:25:27 +0800

    xin0324@:first init support service

---
+++ xl050224-support-bakc/src/main/java/com/xuxin/xl050224supportbakc/Xl050224SupportBakcApplication.java
@@ -0,0 +1,13 @@
+package com.xuxin.xl050224supportbakc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xl050224SupportBakcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Xl050224SupportBakcApplication.class, args);
+    }
+
+}
---
+++ xl050224-support-bakc/src/test/java/com/xuxin/xl050224supportbakc/Xl050224SupportBakcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuxin.xl050224supportbakc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xl050224SupportBakcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
